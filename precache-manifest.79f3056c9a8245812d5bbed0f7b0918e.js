bf07a054a8c34c1ceb48fa81f30c89ce686e98d3
pushpa <pushpa.k@boppotechnologies.com>
2020-02-05 18:24:29 +0530

    Updates

---
+++ precache-manifest.79f3056c9a8245812d5bbed0f7b0918e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5171a5567335d0ad8a038dad1bbea051",
+    "url": "/trailcodebase/index.html"
+  },
+  {
+    "revision": "a11593fb167318b2cf76",
+    "url": "/trailcodebase/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ef9804a867f77f9e15ac",
+    "url": "/trailcodebase/static/js/2.ec1606be.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/trailcodebase/static/js/2.ec1606be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a11593fb167318b2cf76",
+    "url": "/trailcodebase/static/js/main.e7fa129b.chunk.js"
+  },
+  {
+    "revision": "43f799e855cb117ef1a6",
+    "url": "/trailcodebase/static/js/runtime-main.650a5797.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trailcodebase/static/media/logo.5d5d9eef.svg"
+  }
+]);
